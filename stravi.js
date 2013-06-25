709f677825832ea45a0add08801d60121340bac5
Peter <pverboom@gmail.com>
2013-06-25 17:29:11 +0200

    Database, terrainGenerator

--- stravi.js
+++ stravi.js
@@ -1,11 +1,29 @@
+/*
+Accounts.loginServiceConfiguration.insert({
+  service: "facebook",
+  appId: "366989566757303",
+  secret: "230fea9aa0734a8ccc1c4aea6fdfe1fb"
+});
+*/
+
+tileCollection = new Meteor.Collection('tiles');
+
 if (Meteor.isClient) {
-	Meteor.startup(function () {
-		g = new Stravi.Game();
-	});
+    Meteor.startup(function () {
+        
+        Meteor.subscribe('tiles', function() {
+            console.log(tileCollection.find().count());
+        });
+        
+        g = new Stravi.Game();
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+        Meteor.publish('tiles', function() {
+            return tileCollection.find();
+        });
+    });
 }
